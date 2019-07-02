8d0c9af74924d4d9d80c06845e0c2fad130d906b
Chandan Jain <c.jainn@ymail.com>
2019-07-02 17:01:23 +0530

    removed dotenv

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-require('dotenv').config();
+//require('dotenv').config();
 
 const app = require('./app');
 const debug = require('debug')('node-angular');
 const http = require('http');
 
 const normalizePort = val => {
-  var port = parseInt(val, 10);
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
+	var port = parseInt(val, 10);
+	if (isNaN(port)) {
+		return val;
+	}
+	if (port >= 0) {
+		return port;
+	}
+	return false;
 };
 
 const onError = error => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + port;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
+	const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + port;
+	switch (error.code) {
+		case 'EACCES':
+			console.error(bind + ' requires elevated privileges');
+			process.exit(1);
+			break;
+		case 'EADDRINUSE':
+			console.error(bind + ' is already in use');
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
 };
 
 const onListening = () => {
-  const addr = server.address();
-  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + port;
-  debug('Listening on ' + bind);
+	const addr = server.address();
+	const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + port;
+	debug('Listening on ' + bind);
 };
 
 const port = normalizePort(process.env.PORT);
@@ -48,5 +48,5 @@ server.on('error', onError);
 server.on('listening', onListening);
 
 server.listen(port, () => {
-  console.log('Server is running at port', port);
+	console.log('Server is running at port', port);
 });
